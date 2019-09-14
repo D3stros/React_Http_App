0cbe2d1c512c5c827bcf838ee31341caa20f0ced
Destros <patrick.fallmann@hotmail.com>
2019-09-15 00:27:05 +0200

    Logging errors via a web platform sentry.io

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import * as Sentry from "@sentry/browser";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+
+Sentry.init({
+  dsn: "https://34691a7aef334e418d957eba69143097@sentry.io/1727336"
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
